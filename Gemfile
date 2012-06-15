4d845a0264ff4142b6972559b197fa3af43bf3f8
Anton S <ijon57@gmail.com>
2012-06-15 10:41:38 +0200

    generated new rails app added gems to Gemfile generated files for cucumber generated files for rspec

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'sqlite3'
+gem 'paperclip'
+gem 'authlogic'
+
+
+group :test do
+  gem 'factory_girl'
+  gem 'rspec-rails'
+  gem 'cucumber-rails'
+  gem 'capybara'
+  gem 'database_cleaner'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleMusicDb::Application.config.secret_token = 'fcc6a9f9791fe89b96ef791151a0b398035d6592cd8244c9ea9b40ee71178ceaf8cffdf3423c4376fdf28924114b2c52c18ed7393e01fb574a883f65a610e63a'
